e2e5f8e3954cb7814738f7ada0af49017f638e21
casjay <git-admin@casjaysdev.com>
2021-08-20 13:22:13 -0400

    Update plugins.zsh

--- etc/plugins.zsh
+++ etc/plugins.zsh
@@ -1,2 +1,12 @@
-PLUGDIR="$HOME/.local/share/zsh"
+# List of plugins/themes to clone/update
 
+if [ -d "$HOME/.local/share/zsh/oh-my-zsh/custom/plugins/zsh-syntax-highlighting/.git" ]; then
+   git -C "$HOME/.local/share/zsh/oh-my-zsh/custom/plugins/zsh-syntax-highlighting" pull -q &>/dev/null
+else
+   git clone https://github.com/zsh-users/zsh-syntax-highlighting "$HOME/.local/share/zsh/oh-my-zsh/custom/plugins/zsh-syntax-highlighting" &>/dev/null
+fi
+if [ -d "$HOME/.local/share/zsh/oh-my-zsh/custom/themes/powerlevel10k/.git" ]; then
+   git -C "$HOME/.local/share/zsh/oh-my-zsh/custom/themes/powerlevel10k" pull &>/dev/null
+else
+   git clone https://github.com/romkatv/powerlevel10k.git "$HOME/.local/share/zsh/oh-my-zsh/custom/themes/powerlevel10k" &>/dev/null
+fi
