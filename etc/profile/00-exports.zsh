7992a4d7cb3915cca655c65e88938a9d0883eba7
casjay <git-admin@casjaysdev.com>
2021-08-20 11:57:12 -0400

    Update 00-exports.zsh

--- etc/profile/00-exports.zsh
+++ etc/profile/00-exports.zsh
@@ -3,6 +3,7 @@ export ZDOTDIR="$HOME/.config/zsh"
 export ZSH_CACHEDIR="$HOME/.cache/oh-my-zsh"
 export ZSH="$HOME/.local/share/zsh/oh-my-zsh"
 export ZSH_CUSTOM="$HOME/.local/share/zsh/oh-my-zsh/custom"
+export ZPLUG_HOME="$HOME/.local/share/zsh/zplug" 
 export HISTFILE="$HOME/.cache/zhistory"
 export SAVEHIST=5000
 export HISTSIZE=2000
