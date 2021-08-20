9bc43abf9b4eb7c098fdb761daf61314d132269c
casjay <git-admin@casjaysdev.com>
2021-08-20 12:00:50 -0400

    Update 00-zplug.zsh

--- etc/profile/00-zplug.zsh
+++ etc/profile/00-zplug.zsh
@@ -1,8 +1,13 @@
 #!/usr/bin/env/zsh
 
+# Load init script
 source "$HOME/.local/share/zsh/zplug/init.zsh"
+
+# Define plugins
 zplug "DhavalKapil/luaver"
 
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# Run last
 if ! zplug check --verbose; then
     printf "Install? [y/N]: "
     if read -q; then
