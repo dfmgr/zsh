4b7def6edea139b733018dd6d5e906723f8acba6
Jason <jason@casjay.net>
2021-10-27 22:50:05 -0400

    🗃 Modified: etc/install_plugins.zsh etc/profile/80-omz.zsh  🗃

--- etc/install_plugins.zsh
+++ etc/install_plugins.zsh
@@ -1,29 +1,33 @@
 # List of plugins/themes to clone/update
 if [[ -f "$HOME/.config/zsh/profile/00-exports.zsh" ]]; then
-   source "$HOME/.config/zsh/profile/00-exports.zsh"
+  source "$HOME/.config/zsh/profile/00-exports.zsh"
 fi
 
 if [[ ! -d "$HOME/.local/share/zsh/oh-my-zsh/.git" ]]; then
-   echo "Installing oh-my-sh plugin and themes"
-   git clone -q "https://github.com/robbyrussell/oh-my-zsh" "$HOME/.local/share/zsh/oh-my-zsh"
+  echo "Installing oh-my-sh plugin and themes"
+  git clone -q "https://github.com/robbyrussell/oh-my-zsh" "$HOME/.local/share/zsh/oh-my-zsh"
 fi
 
 if [[ ! -d "$HOME/.local/share/zsh/oh-my-zsh/custom/plugins/zsh-syntax-highlighting/.git" ]]; then
-   echo "zsh-syntax-highlighting"
-   git clone -q "https://github.com/zsh-users/zsh-syntax-highlighting" "$HOME/.local/share/zsh/oh-my-zsh/custom/plugins/zsh-syntax-highlighting" &>/dev/null
+  echo "zsh-syntax-highlighting"
+  git clone -q "https://github.com/zsh-users/zsh-syntax-highlighting" "$HOME/.local/share/zsh/oh-my-zsh/custom/plugins/zsh-syntax-highlighting" &>/dev/null
+fi
+
+if [[ ! -d "$ZSH/custom/themes/powerlevel9k/.git" ]]; then
+  git clone -q https://github.com/bhilburn/powerlevel9k.git "$ZSH/custom/themes/powerlevel9k"
 fi
 
 if [[ ! -d "$HOME/.local/share/zsh/oh-my-zsh/custom/themes/powerlevel10k/.git" ]]; then
-   echo "Installing powerlevel10k"
-   git clone -q "https://github.com/romkatv/powerlevel10k.git" "$HOME/.local/share/zsh/oh-my-zsh/custom/themes/powerlevel10k" &>/dev/null
+  echo "Installing powerlevel10k"
+  git clone -q "https://github.com/romkatv/powerlevel10k.git" "$HOME/.local/share/zsh/oh-my-zsh/custom/themes/powerlevel10k" &>/dev/null
 fi
 
 if [[ ! -d "$HOME/.local/share/zsh/zsh-snap/.git" ]]; then
-   echo "Installing zsh-snap"
-   git clone -q "https://github.com/marlonrichert/zsh-snap" "$HOME/.local/share/zsh/zsh-snap" &>/dev/null
+  echo "Installing zsh-snap"
+  git clone -q "https://github.com/marlonrichert/zsh-snap" "$HOME/.local/share/zsh/zsh-snap" &>/dev/null
 fi
 
 if [[ ! -d "$HOME/.local/share/zsh/zplug" ]]; then
-   echo "Installing zplug"
-   git clone -q "https://github.com/zplug/zplug" "$HOME/.local/share/zsh/zplug" &>/dev/null
+  echo "Installing zplug"
+  git clone -q "https://github.com/zplug/zplug" "$HOME/.local/share/zsh/zplug" &>/dev/null
 fi
--- etc/profile/80-omz.zsh
+++ etc/profile/80-omz.zsh
@@ -17,9 +17,9 @@ fi
 if [[ ! -d "$ZSH/custom/themes/powerlevel10k/.git" ]]; then
   git clone -q https://github.com/romkatv/powerlevel10k.git "$ZSH/custom/themes/powerlevel10k"
 fi
-#if [[ ! -d "$ZSH/custom/themes/powerlevel9k/.git" ]]; then
-#git clone -q https://github.com/bhilburn/powerlevel9k.git "$ZSH/custom/themes/powerlevel9k"
-#fi
+if [[ ! -d "$ZSH/custom/themes/powerlevel9k/.git" ]]; then
+  git clone -q https://github.com/bhilburn/powerlevel9k.git "$ZSH/custom/themes/powerlevel9k"
+fi
 
 # oh-my-zsh cache dir
 if [[ ! -d "$HOME/.cache/oh-my-zsh" ]]; then
@@ -45,18 +45,18 @@ esac
 if [[ -f "$ZSH/oh-my-zsh.sh" ]]; then
   source "$ZSH/oh-my-zsh.sh" 2>/dev/null
 fi
+# load theme powerlevel10k
+if [[ -f "$ZSH/custom/themes/powerlevel10k/powerlevel10k.zsh-theme" ]]; then
+  source "$ZSH/custom/themes/powerlevel10k/powerlevel10k.zsh-theme" 2>/dev/null
+  source "$HOME/.config/zsh/themes/powerlevel-10k.zsh"
+  ZSH_THEME="powerlevel10k/powerlevel10k"
+fi
 # load theme powerlevel9k
 if [[ -f "$ZSH/custom/themes/powerlevel9k/powerlevel9k.zsh-theme" ]]; then
   source "$ZSH/custom/themes/powerlevel9k/powerlevel9k.zsh-theme" 2>/dev/null
   source "$HOME/.config/zsh/themes/powerlevel-09k.zsh"
   ZSH_THEME="powerlevel9k/powerlevel9k"
 fi
-# load theme powerlevel10k
-#if [[ -f "$ZSH/custom/themes/powerlevel10k/powerlevel10k.zsh-theme" ]]; then
-#    source "$ZSH/custom/themes/powerlevel10k/powerlevel10k.zsh-theme" 2>/dev/null
-#    source "$HOME/.config/zsh/themes/powerlevel-10k.zsh"
-#    ZSH_THEME="powerlevel10k/powerlevel10k"
-#fi
 # Uncomment the following line to change how often to auto-update (in days).
 export UPDATE_ZSH_DAYS=30
 # Load plugins
