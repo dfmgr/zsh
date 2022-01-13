b5175029a1567d69fb1d1ec46a76cc632ee747db
Jason <git-admin@casjaysdev.com>
2022-01-13 13:26:37 -0500

    🗃 Modified: etc/install_plugins.zsh etc/zshrc  🗃

--- etc/install_plugins.zsh
+++ etc/install_plugins.zsh
@@ -2,20 +2,50 @@
 if [[ -f "$HOME/.config/zsh/profile/00-exports.zsh" ]]; then
   source "$HOME/.config/zsh/profile/00-exports.zsh"
 fi
+## Update plugins
+if [[ -d "$HOME/.local/share/zsh/oh-my-zsh/.git" ]]; then
+  echo "Updating oh-my-sh plugin and themes"
+  git -C "$HOME/.local/share/zsh/oh-my-zsh" pull &>/dev/null
+fi
 
+if [[ -d "$HOME/.local/share/zsh/oh-my-zsh/custom/plugins/zsh-syntax-highlighting/.git" ]]; then
+  echo "Updating zsh-syntax-highlighting"
+  git -C "$HOME/.local/share/zsh/oh-my-zsh/custom/plugins/zsh-syntax-highlighting" pull &>/dev/null
+fi
+
+if [[ -d "$HOME/.local/share/zsh/oh-my-zsh/custom/themes/powerlevel9k/.git" ]]; then
+  echo "Updating powerlevel9k"
+  git -C "$HOME/.local/share/zsh/oh-my-zsh/custom/themes/powerlevel9k" pull &>/dev/null
+fi
+
+if [[ -d "$HOME/.local/share/zsh/oh-my-zsh/custom/themes/powerlevel10k/.git" ]]; then
+  echo "Updating powerlevel10k"
+  git -C "$HOME/.local/share/zsh/oh-my-zsh/custom/themes/powerlevel10k" pull &>/dev/null
+fi
+
+if [[ -d "$HOME/.local/share/zsh/zsnap/.git" ]]; then
+  echo "Updating zsh-snap"
+  git -C "$HOME/.local/share/zsh/zsnap" pull &>/dev/null
+fi
+
+if [[ -d "$HOME/.local/share/zsh/zplug/.git" ]]; then
+  echo "Updating zplug"
+  git -C "$HOME/.local/share/zsh/zplug" pull &>/dev/null
+fi
+## Install plugins
 if [[ ! -d "$HOME/.local/share/zsh/oh-my-zsh/.git" ]]; then
   echo "Installing oh-my-sh plugin and themes"
-  git clone -q "https://github.com/robbyrussell/oh-my-zsh" "$HOME/.local/share/zsh/oh-my-zsh"
+  git clone -q "https://github.com/robbyrussell/oh-my-zsh" "$HOME/.local/share/zsh/oh-my-zsh" &>/dev/null
 fi
 
 if [[ ! -d "$HOME/.local/share/zsh/oh-my-zsh/custom/plugins/zsh-syntax-highlighting/.git" ]]; then
-  echo "zsh-syntax-highlighting"
+  echo "Installing zsh-syntax-highlighting"
   git clone -q "https://github.com/zsh-users/zsh-syntax-highlighting" "$HOME/.local/share/zsh/oh-my-zsh/custom/plugins/zsh-syntax-highlighting" &>/dev/null
 fi
 
 if [[ ! -d "$HOME/.local/share/zsh/oh-my-zsh/custom/themes/powerlevel9k/.git" ]]; then
   echo "Installing powerlevel9k"
-  git clone -q "https://github.com/bhilburn/powerlevel9k.git" "$HOME/.local/share/zsh/oh-my-zsh/custom/themes/powerlevel9k"
+  git clone -q "https://github.com/bhilburn/powerlevel9k.git" "$HOME/.local/share/zsh/oh-my-zsh/custom/themes/powerlevel9k" &>/dev/null
 fi
 
 if [[ ! -d "$HOME/.local/share/zsh/oh-my-zsh/custom/themes/powerlevel10k/.git" ]]; then
@@ -28,7 +58,7 @@ if [[ ! -d "$HOME/.local/share/zsh/zsnap/.git" ]]; then
   git clone -q "https://github.com/marlonrichert/zsh-snap" "$HOME/.local/share/zsh/zsnap" &>/dev/null
 fi
 
-if [[ ! -d "$HOME/.local/share/zsh/zplug" ]]; then
+if [[ ! -d "$HOME/.local/share/zsh/zplug/.git" ]]; then
   echo "Installing zplug"
   git clone -q "https://github.com/zplug/zplug" "$HOME/.local/share/zsh/zplug" &>/dev/null
 fi
