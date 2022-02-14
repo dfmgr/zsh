a1128ac68ac3a7ff908df68afe13bcc2411361dc
Jason <git-admin@casjaysdev.com>
2022-02-13 22:17:16 -0500

    🗃️ modified: etc/plugins/zsnap.zsh 🗃️

--- etc/plugins/zsnap.zsh
+++ etc/plugins/zsnap.zsh
@@ -1,8 +1,10 @@
 #!/usr/bin/env zsh
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 # zsnap install
+ZNAP_ENABLED="${ZNAP_ENABLED:-false}"
 ZSNAP_HOME="${ZSNAP_HOME:-$HOME/.local/share/zsh/zsnap}"
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+[[ "$ZNAP_ENABLED" = "true" ]] || return 0
 if [[ ! -d "$ZSNAP_HOME/.git" ]]; then
   git clone "https://github.com/marlonrichert/zsh-snap" "$ZSNAP_HOME" &>/dev/null
 fi
