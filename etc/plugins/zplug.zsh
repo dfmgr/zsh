4f947cea1082ede6db8f0a7fd7dd6feecf1d4a5c
Jason <git-admin@casjaysdev.com>
2022-02-13 22:17:16 -0500

    🗃️ modified: etc/plugins/zplug.zsh 🗃️

--- etc/plugins/zplug.zsh
+++ etc/plugins/zplug.zsh
@@ -1,8 +1,10 @@
 #!/usr/bin/env zsh
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 # zplug install
+ZPLUG_ENABLED="${ZPLUG_ENABLED:-false}"
 ZPLUG_HOME="${ZPLUG_HOME:-$HOME/.local/share/zsh/zplug}"
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+[[ "$ZPLUG_ENABLED" = "true" ]] || return 0
 if [[ ! -d "$ZPLUG_HOME/.git" ]]; then
   git clone "https://github.com/zplug/zplug" "$ZPLUG_HOME" &>/dev/null
 fi
